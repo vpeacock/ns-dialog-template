59ad9da57d6f2c3bf32bcdb1beac015e7da11a73
[Valentina] <[mariaiscus1@gmail.com]>
2024-05-01 17:10:02 +0500

    add files

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,10 +6,11 @@ import {
 
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./components/home/home.component";
+import { DialogComponent } from "./components/dialog/dialog.component";
 
 @NgModule({
   imports: [NativeScriptCommonModule, HomeRoutingModule],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, DialogComponent],
   providers: [NativeDialogService],
   schemas: [NO_ERRORS_SCHEMA],
 })
